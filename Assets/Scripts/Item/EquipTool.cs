296e2b265bdeaaa3ebdfcd04e7a09a97b780e823
Sage-Teshi <dua2670@gmail.com>
2025-05-23 14:18:24 +0900

    feat: 무기 애니메이션 추가
    - 무기 애니메이션 추가

--- Assets/Scripts/Item/EquipTool.cs
+++ Assets/Scripts/Item/EquipTool.cs
@@ -8,6 +8,7 @@ public class EquipTool : Equip
     public float attackRate;
     private bool attacking;
     public float attackDistance;
+    public float useStamina;
 
     [Header("Resource Gathering")]
     public bool doesGatherResources;
@@ -17,10 +18,12 @@ public class EquipTool : Equip
     public int damage;
 
     private Animator animator;
+    private Camera camera;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        camera = Camera.main;
     }
 
 
@@ -28,14 +31,32 @@ public class EquipTool : Equip
     {
         if (!attacking)
         {
-            attacking = true;
-            animator.SetTrigger("Attack");
-            Invoke("OnCanAttack", attackRate);
+            if (CharacterManager.Instance.Player.condition.UseStamina(useStamina))
+            { 
+                attacking = true;
+                animator.SetTrigger("Attack");
+                Invoke("OnCanAttack", attackRate);
+            }
         }
     }
+
     void OnCanAttack()
     {
         attacking = false;
+    }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+        }
     }
 
 }
